e26e94fdbb46c018a98061c8cadc61387112ed17
Shubham SUnny <shubhamsahaninitkkr@gmail.com>
2018-05-24 15:31:52 +0530

    Revert "integration complete"
    This reverts commit ddaea676797547c5d74eb9a4f393c3cfec1f0963.

--- js/map.js
+++ js/map.js
@@ -17,22 +17,11 @@ $.ajax({
     if(data.hasOwnProperty(key)) {
     var value = data[key];
     var i=0;
-
-
-    var styles = [ { "featureType": "administrative", "elementType": "labels.text.fill", "stylers": [ { "color": "#444444" } ] }, { "featureType": "landscape", "elementType": "all", "stylers": [ { "color": "#FFFFFF" } ] }, { "featureType": "poi", "elementType": "all", "stylers": [ { "visibility": "on" } ] }, { "featureType": "poi", "elementType": "geometry.fill", "stylers": [ { "saturation": "-100" }, { "lightness": "57" } ] }, { "featureType": "poi", "elementType": "geometry.stroke", "stylers": [ { "lightness": "1" } ] }, { "featureType": "poi", "elementType": "labels", "stylers": [ { "visibility": "off" } ] }, { "featureType": "road", "elementType": "all", "stylers": [ { "saturation": -100 }, { "lightness": 45 } ] }, { "featureType": "road.highway", "elementType": "all", "stylers": [ { "visibility": "simplified" } ] }, { "featureType": "road.arterial", "elementType": "labels.icon", "stylers": [ { "visibility": "off" } ] }, { "featureType": "transit", "elementType": "all", "stylers": [ { "visibility": "off" } ] }, { "featureType": "transit.station.bus", "elementType": "all", "stylers": [ { "visibility": "on" } ] }, { "featureType": "transit.station.bus", "elementType": "labels.text.fill", "stylers": [ { "saturation": "0" }, { "lightness": "0" }, { "gamma": "1.00" }, { "weight": "1" } ] }, { "featureType": "transit.station.bus", "elementType": "labels.icon", "stylers": [ { "saturation": "-100" }, { "weight": "1" }, { "lightness": "0" } ] }, { "featureType": "transit.station.rail", "elementType": "all", "stylers": [ { "visibility": "on" } ] }, { "featureType": "transit.station.rail", "elementType": "labels.text.fill", "stylers": [ { "gamma": "1" }, { "lightness": "40" } ] }, { "featureType": "transit.station.rail", "elementType": "labels.icon", "stylers": [ { "saturation": "-100" }, { "lightness": "30" } ] }, { "featureType": "water", "elementType": "all", "stylers": [ { "color": "#33B3F9" }, { "visibility": "on" } ] } ];
-
-   //Styles End
-      //Create a styled map using the above stylessea
-    var styledMap = new google.maps.StyledMapType(styles,{name: "Styled Map"});
-
     var map = new google.maps.Map(document.getElementById('map'), {
       zoom: 11,
       center: new google.maps.LatLng(28.619988,77.292355),
       mapTypeId: google.maps.MapTypeId.ROADMAP
     });
-
-    map.mapTypes.set('map_style', styledMap);
-    map.setMapTypeId('map_style');
 
     var infowindow = new google.maps.InfoWindow();
 
